47f049ec272e107f4a1c1ec6718791262971b014
Rattapoom Pethputong <celica2548@gmail.com>
2023-11-29 10:27:53 +0700

    checkpoint4

--- main.py
+++ main.py
@@ -25,6 +25,3 @@ dict['Year'] = '2017'
 table.insert_row(dict)
 table5 = table.filter(lambda x: x['Genre'] == 'Fantasy').aggregate(lambda y: len(y), 'Film')
 print(f'The number of fantasy movie after updating is {table5}')
-
-table6 = table.update_row('Film', 'A Serious Man', 'Year', 2022).filter(lambda x: x['Film'] == 'A Serious Man')
-print(table6)
